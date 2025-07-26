44aee5f7ccd589b5557f7a7745d198b0ad72aafd
hadidi404 <raysonbarnachia@gmail.com>
2025-07-27 04:49:47 +0800

    Improved the UI of the calendar page.

--- lib/event_calendar_page.dart
+++ lib/event_calendar_page.dart
@@ -26,12 +26,13 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
           try {
             final dateTimeParts = event.dateTime.split(' ');
             final date = DateTime.parse(dateTimeParts[0]);
+
             final time = dateTimeParts.length > 1
                 ? TimeOfDay(
                     hour: int.parse(dateTimeParts[1].split(':')[0]),
                     minute: int.parse(dateTimeParts[1].split(':')[1]),
                   )
-                : const TimeOfDay(hour: 9, minute: 0); // default time
+                : const TimeOfDay(hour: 9, minute: 0);
 
             final start = DateTime(
               date.year,
@@ -40,13 +41,15 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
               time.hour,
               time.minute,
             );
-            final end = start.add(const Duration(hours: 1)); // default duration
+            final end = start.add(const Duration(hours: 1));
 
             return Appointment(
               startTime: start,
               endTime: end,
               subject: event.name,
-              color: Colors.blue,
+              color: Colors.deepPurpleAccent,
+              startTimeZone: '',
+              endTimeZone: '',
             );
           } catch (e) {
             return null;
@@ -64,11 +67,25 @@ class _EventCalendarPageState extends State<EventCalendarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Event Calendar")),
+
       body: SfCalendar(
         view: CalendarView.month,
         dataSource: AppointmentDataSource(appointments),
+        todayHighlightColor: Colors.deepPurple,
+        backgroundColor: Colors.grey[100],
+        selectionDecoration: BoxDecoration(
+          color: Colors.deepPurple.withAlpha((0.2 * 255).toInt()),
+          border: Border.all(color: Colors.deepPurple),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        headerStyle: const CalendarHeaderStyle(
+          textAlign: TextAlign.center,
+          textStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
         monthViewSettings: const MonthViewSettings(
           appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+          showAgenda: true,
+          agendaItemHeight: 60,
         ),
       ),
     );
